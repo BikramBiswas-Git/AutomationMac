06d911c29b4903c6695d3e5493b93e4fd9213366
bkrmbswas@gmail.com <codeclouds-bikram@CISPL-IO-199.local>
2024-03-10 16:01:37 +0530

    Started demo project on a ecommerce site

--- BaseTest.java
+++ BaseTest.java
@@ -9,11 +9,12 @@ import com.aventstack.extentreports.ExtentReports;
 import com.aventstack.extentreports.ExtentTest;
 import Test.AbstractComponent.AbstractComponent;
 import Test.BaseComponents.BaseComponentsDemo;
+import Test.Pages.SignUpPage;
+import Test.Pages.SignUpPage;
 
 public class BaseTest extends BaseComponentsDemo
 
 {
-
 	public BaseTest() {
 		// Default constructor
 	}
@@ -23,14 +24,12 @@ public class BaseTest extends BaseComponentsDemo
 	private ExtentReports extent;
 	private ExtentTest test;
 
-	@Test
-	public void Tracking() throws InterruptedException {
-		for (int i = 0; i < 2; i++) {
-			driver.get("https://google.com");
-			System.out.println("URL got hit " + i + "times");
-		}
-		assertEquals(false, true);
+	// Page Objects
+	SignUpPage SignUp = new SignUpPage(driver);
 
+	@Test(priority = 1)
+	public void SignUpPageTest() throws InterruptedException {
+		SignUp.SigningUp();
 	}
 
 }
